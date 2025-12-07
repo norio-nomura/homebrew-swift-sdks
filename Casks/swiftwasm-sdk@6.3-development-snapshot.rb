15fea3c09489e85c74ce1ec7c533ddd3b84b13ff
Norio Nomura <norio.nomura@gmail.com>
2025-12-07 15:36:31 +0900

    swiftwasm-sdk@6.3-development-snapshot 2025-12-02-a

---
+++ Casks/swiftwasm-sdk@6.3-development-snapshot.rb
@@ -0,0 +1,24 @@
+cask "swiftwasm-sdk@6.3-development-snapshot" do
+  version "2025-12-02-a"
+  sha256 "d26d1c3f5b94b4f275f7de039cecf7ab0c6ee129ea47bccb46416a0cba952718"
+
+  release_name="swift-6.3-DEVELOPMENT-SNAPSHOT-#{version}"
+  artifactbundle_name="#{release_name}_wasm.artifactbundle"
+  webroot="swift-6.3-branch"
+
+  url "https://download.swift.org/#{webroot}/wasm-sdk/#{release_name}/#{artifactbundle_name}.tar.gz"
+  name "Swift Wasm SDK 6.3 Development Snapshot"
+  desc "Swift Wasm SDK 6.3 Development Snapshot"
+  homepage "https://www.swift.org/"
+
+  livecheck do
+    url "https://github.com/swiftlang/swift-org-website/raw/refs/heads/main/_data/builds/#{webroot.tr(".", "_")}/wasm-sdk.yml"
+    regex(/swift-(\d+\.\d+-)?DEVELOPMENT-SNAPSHOT-(\d+-\d+-\d+-a)/)
+    strategy :yaml do |yaml, regex|
+      yaml.select { |item| item["dir"]&.match?(regex) }
+          .map { |item| item["dir"][regex, 2] }
+    end
+  end
+
+  artifact artifactbundle_name.to_s, target: "~/Library/org.swift.swiftpm/swift-sdks/#{artifactbundle_name}"
+end
